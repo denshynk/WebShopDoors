9eb048eb58b541cf633133d0ba961b4e11099b9e
Denys Shynkarenko <denshynk@gmail.com>
2023-04-14 18:22:25 +0300

    backend creating

---
+++ index.js
@@ -0,0 +1,95 @@
+import express from 'express';
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcrypt';
+import mongoose from "mongoose";
+import { validationResult } from 'express-validator'
+
+import { registerValidation } from "./validation/auth.js";
+
+import UserModel from './models/User.js'
+
+mongoose.connect(
+    "mongodb+srv://user:12345@cluster0.yezzjjp.mongodb.net/blog?retryWrites=true&w=majority")
+    .then(()=> console.log('DB ok'))
+    .catch((err)=> console.log('DB error', err));
+;
+
+const app = express();
+
+app.use(express.json());
+
+app.post('/auth/login', async (req, res) => {
+    try {
+        const user = await UserModel.findOne({ email: req.body.email });
+
+        if(!user) {
+            return req.status(404).json({
+                massage: "Пользователь не найден(пишим для разроботки)",
+            });
+        }
+        
+        const isValidPass = await bcrypt.compare(req.body.password, user._doc.password);
+
+        if (!isValidPass) {
+            return req.status(404).json({
+                massage: "Не верный логин или пароль",
+            });
+        }
+    } catch (err) {
+        
+    }
+});
+
+app.post("/auth/register", registerValidation, async(req, res) => {
+    try {
+        const errors = validationResult(req);
+    if(!errors.isEmpty()) {
+        return res.status(400).json(errors.array());
+    }
+
+    const password = req.body.password;
+    const salt = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hash(password, salt);
+
+    const doc = new UserModel({
+        email: req.body.email,
+        fullName: req.body.fullName,
+        avatarUrl: req.body.avatarUrl,
+        passwordHash: hash,
+    });
+
+    const user = await doc.save()
+
+    const token = jwt.sign(
+        {
+        _id: user._id,
+        }, 
+        'secret123', 
+        {
+            expiresIn: '30'
+        },
+    );
+    
+    const { passwordHash, ...userData } = user._doc;
+
+    res.json({
+        ...userData,
+        token,
+    });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({
+            message: "Не удалось зарегистрироваться",
+        });
+    }
+});
+
+
+
+app.listen(4444, (err) => {
+    if (err) {
+        return console.log(err); 
+    }
+
+    console.log('Server OK');
+});
